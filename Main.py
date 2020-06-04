79d1ef3cc28b5326ab77e7fa6a5f14cd821d04ef
EvanBlaine <56650568+EvanBlaine@users.noreply.github.com>
2020-06-04 15:53:14 -0500

    Update Main.py
    Added logging idea to Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-print("Hello Team!")
+import discord, json, logging
+from discord.ext import commands
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter('{asctime}:{levelname}:{name}:{message}', style='{'))
+log.addHandler(handler)
+
+logging.getLogger('discord').setLevel(logging.WARNING)
