488d953afdc9e611e63e0c0e515a0abc6337418f
evanblaine <evanblaineadams@gmail.com>
2020-06-11 12:00:52 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
-print("Hello Team!")
+import os, json, discord, logging
+
+import discord.ext.commands as commands
+
+class Main(commands.Bot):
+    
+    def __init__(self):
+        self.config = 'config.json'
+        try:
+            with open(self.config) as fp:
+                self.config = json.load(fp)
+            print("Found config.json!")
+        except:
+            print("Could not find config.json!")
+            
+        super().__init__(commands.when_mentioned_or(self.config['prefix']), description="Hello I'm OD1!")
+
+        for file in os.listdir("cogs"):
+            if file.endswith(".py"):
+                name = file[:-3]
+                self.load_extension(f"cogs.{name}")
+
+bot = Main()
+bot.run(bot.config['token'])
