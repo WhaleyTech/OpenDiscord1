95266c96bd79966459d78d676a6b102bbf8cdba4
evanblaine <evanblaineadams@gmail.com>
2020-06-16 17:43:19 -0500

    Add Help.py under cogs/

---
+++ cogs/Help.py
@@ -0,0 +1,35 @@
+import discord
+import discord.ext.commands as commands
+
+#Need to put in another python file or switch up to json
+addme = ['addme', 'Shares a link that allows to join your own discord server']
+coinflip = ['coinflip', 'Flips a coin!']
+
+cogs = [addme, coinflip]
+#----------
+
+def setup(bot):
+    bot.remove_command('help')
+    bot.add_cog(Help(bot))
+
+class Help(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.info_title = 'All commands'
+        self.info_msg = '▫ **command** [alias1, alias2, ...] - `description`\n\n'
+
+        for cog in cogs:
+            aliases = ''
+            if len(cog) is 3:
+                aliases += f' [{cog[2]}]'
+            self.info_msg += f'▫ **{cog[0]}**{aliases} - ``{cog[1]}``\n'
+   
+    @commands.command()
+    async def help(self, ctx):
+        info_embed = discord.Embed(
+            title = self.info_title,
+            description = self.info_msg,
+            color=0x2C2F33
+        )
+        await ctx.send(embed=info_embed)
