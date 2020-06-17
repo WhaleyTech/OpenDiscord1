9cb38ccd2eb6b49fa9852a1a0e63c668c4151675
evanblaine <evanblaineadams@gmail.com>
2020-06-17 14:22:07 -0500

    Change embed color and structure of Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -17,19 +17,19 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.info_title = 'All commands'
-        self.info_msg = '▫ **command** [alias1, alias2, ...] - `description`\n\n'
+        self.info_msg = '▫ **command** [alias1, alias2, ...] - description\n\n'
 
         for cog in cogs:
             aliases = ''
             if len(cog) is 3:
                 aliases += f' [{cog[2]}]'
-            self.info_msg += f'▫ **{cog[0]}**{aliases} - ``{cog[1]}``\n'
+            self.info_msg += f'▫ **{cog[0]}**{aliases} - {cog[1]}\n'
    
     @commands.command()
     async def help(self, ctx):
         info_embed = discord.Embed(
             title = self.info_title,
             description = self.info_msg,
-            color=0x2C2F33
+            color=0xE91E63 #LUMINOUS_VIVID_PINK
         )
         await ctx.send(embed=info_embed)
