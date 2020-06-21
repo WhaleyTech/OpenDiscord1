f3c7840e37064bbf9c3500762eafd7257da14b0a
evanblaine <evanblaineadams@gmail.com>
2020-06-21 12:29:22 -0500

    Update HelpList.py

--- utilities/HelpList.py
+++ utilities/HelpList.py
@@ -1,3 +1,5 @@
+#'list' = ['command alias', 'description about the command']  --- add command list to 'cogs'
+
 addme = ['addme', 'Shares a link that allows to join your own discord server']
 coinflip = ['coinflip', 'Flips a coin!']
 
