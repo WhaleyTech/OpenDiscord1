e724ee810245b83d4f34cdc294c10b8d9f223aab
evanblaine <evanblaineadams@gmail.com>
2020-07-01 12:33:09 -0500

    Modify command alias in HelpList.py

--- utilities/HelpList.py
+++ utilities/HelpList.py
@@ -1,6 +1,6 @@
 #'list' = ['command alias', 'description about the command']  --- add command list to 'cogs'
 
-addme = ['addme', 'Shares a link that allows to join your own discord server']
-coinflip = ['coinflip', 'Flips a coin!']
+addme = ['!addme', 'Shares a link that allows to join your own discord server']
+coinflip = ['!coinflip', 'Flips a coin!']
 
 cogs = [addme, coinflip]
