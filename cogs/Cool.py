ece88cb56adc1eab8a36e2a9522090ef138aea63
evanblaine <evanblaineadams@gmail.com>
2020-07-06 01:30:49 -0500

    Add new command 'roll'

--- cogs/Cool.py
+++ cogs/Cool.py
@@ -1,5 +1,6 @@
 import random
 import discord
+import time
 
 import discord.ext.commands as commands
 
@@ -12,7 +13,13 @@ class Cool(commands.Cog):
     async def coinflip(self, ctx):
         coin = ['Tails', 'Heads']
         await ctx.send(f"You flipped a coin! **{random.choice(coin)}**")
-        
+
+    @commands.command()
+    async def roll(self, ctx):
+        dice_min=1
+        dice_max=6
+        await ctx.send(f':game_die: You roll a die! **{random.randint(dice_min, dice_max)}**')     
+
 
 def setup(bot):
     bot.add_cog(Cool(bot))
