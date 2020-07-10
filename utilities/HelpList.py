deb5aa3bf5c71a8ae3ea43180a4efa03fc5f3f58
evanblaine <evanblaineadams@gmail.com>
2020-07-10 10:32:06 -0500

    Update HelpList.py

--- utilities/HelpList.py
+++ utilities/HelpList.py
@@ -1,6 +1,7 @@
 #'list' = ['command alias', 'description about the command']  --- add command list to 'cogs'
 
-addme = ['addme', 'Shares a link that allows Opendiscord1 to join your own discord server']
+addme = ['addme, invite', 'Shares a link that allows Opendiscord1 to join your own discord server']
+commits = ['commits, commit', 'Shows the latest commit history pushed to github']
 coinflip = ['coinflip', 'Flips a coin!']
 rolldice = ['rolldice', 'Rolls a die :game_die:']
 uppercase = ['uppercase', 'Makes the message uppercase']
@@ -8,4 +9,4 @@ lowercase = ['lowercase', 'Makes your message lowercase']
 reverse = ['reverse', '!sdrow ruoy sesreveR']
 eightball = ["eightball, 8ball, 8, ball", "The magic eight ball! :crystal_ball:"]
 
-cogs = [addme, coinflip, rolldice, uppercase, lowercase, reverse, eightball]
+cogs = [addme, commits, coinflip, rolldice, uppercase, lowercase, reverse, eightball]
