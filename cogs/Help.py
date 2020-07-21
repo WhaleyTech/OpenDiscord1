f2e755f7d72c86fac0e228ed973f90ee9ccf7ccf
evanblaine <evanblaineadams@gmail.com>
2020-07-21 12:26:12 -0500

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -11,20 +11,25 @@ class Help(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.info_title = 'All commands'
-        self.info_msg = '▫ **command** [alias1, alias2, ...] - description\n\n'
+        self.title = '_All commands_'
+        self.msg = ''
 
         for cog in hl.cogs:
             aliases = ''
             if len(cog) is 3:
                 aliases += f' [{cog[2]}]'
-            self.info_msg += f'▫ **{cog[0]}**{aliases} - {cog[1]}\n'
+            self.msg += f'- **_{cog[0]}_**{aliases} - {cog[1]}\n'
    
     @commands.command()
     async def help(self, ctx):
-        info_embed = discord.Embed(
-            title = self.info_title,
-            description = self.info_msg,
+        embed = discord.Embed(
+            title = self.title,
+            description = self.msg,
             color=0xE91E63 #LUMINOUS_VIVID_PINK
         )
-        await ctx.send(embed=info_embed)
+        embed.set_thumbnail(url="https://img.icons8.com/color/48/000000/python.png")
+        embed.add_field(name="_Support_", value="If you have any issues with the bot, please go to our " + 
+            "[Github Issues Page](https://github.com/WhaleyTech/OpenDiscord1/issues) and create a new issue")
+        embed.add_field(name="_Add to Discord_", value="This bot can be added to your very own server! " +
+        "[Click here to add it to yours]({})".format(discord.utils.oauth_url(self.bot.user.id)), inline=False)
+        await ctx.send(embed=embed)
