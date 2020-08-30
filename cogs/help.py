6aa3970911bf9776cfe596eb5e019282b6ee3bd6
evanblaine <evanblaineadams@gmail.com>
2020-08-30 13:10:33 -0500

    Update help command
    Added help1, help2, help3 and made
    a few changes to help.py and helplist.py

--- cogs/help.py
+++ cogs/help.py
@@ -7,6 +7,14 @@ def setup(bot):
     bot.remove_command('help')
     bot.add_cog(Help(bot))
 
+def c_(n: list, msg: str):
+        for i in n:
+            aliases = ''
+            if len(i) is 3:
+                aliases += f' [{i[2]}]'
+            msg += f'- **_{i[0]}_**{aliases} - {i[1]}\n'
+        return msg
+
 class Help(commands.Cog):
 
     def __init__(self, bot):
@@ -14,18 +22,12 @@ class Help(commands.Cog):
         self.title = f'_{self.bot.config["name"]} Commands_'
         self.msg = ''
 
-        for cog in hl.cogs:
-            aliases = ''
-            if len(cog) is 3:
-                aliases += f' [{cog[2]}]'
-            self.msg += f'- **_{cog[0]}_**{aliases} - {cog[1]}\n'
-   
     @commands.command()
     async def help(self, ctx):
         embed = discord.Embed(
             title = self.title,
-            description = self.msg,
-            color=0xE91E63 #LUMINOUS_VIVID_PINK
+            description = "Use **{0}help1 | {0}help2 | {0}help3** for a list of commands!".format(self.bot.config["prefix"]),
+            color = 0xE91E63, #LUMINOUS_VIVID_PINK
         )
         embed.set_thumbnail(url="https://img.icons8.com/color/48/000000/python.png")
         embed.add_field(name="_Support_", value="If you have any issues with the bot, please go to our " + 
@@ -33,3 +35,35 @@ class Help(commands.Cog):
         embed.add_field(name="_Add to Discord_", value="This bot can be added to your very own server! " +
         "[Click here to add it to yours]({})".format(discord.utils.oauth_url(self.bot.user.id)), inline=False)
         await ctx.send(embed=embed)
+
+    @commands.command()
+    async def help1(self, ctx):
+        info_embed = discord.Embed(
+            title = "_Help1 - Info Commands_",
+            description = c_(n=hl.info_cmds, msg=self.msg),
+            color = 0xE91E63,
+        )
+        info_embed.set_footer(text="{0}help2 - Admin Commands | {0}help3 - Random Commands".format(self.bot.config["prefix"]))
+        await ctx.send(embed=info_embed)
+
+    @commands.command()
+    async def help2(self, ctx):
+        """admin commands"""
+        admin_embed = discord.Embed(
+            title = "_Help3 - Admin Commands_",
+            description = c_(n=hl.admin_cmds, msg=self.msg),
+            color = 0xE91E63,
+        )
+        admin_embed.set_footer(text="{0}help3 - Random Commands".format(self.bot.config["prefix"]))
+        await ctx.send(embed=admin_embed)
+    
+    @commands.command()
+    async def help3(self, ctx):
+        """random commands"""
+        rand_embed = discord.Embed(
+            title = "_Help2 - Random Commands_",
+            description = c_(n=hl.rand_cmds, msg=self.msg),
+            color = 0xE91E63,
+        )
+        rand_embed.set_footer(text="End of Help!")
+        await ctx.send(embed=rand_embed)
--- utilities/helplist.py
+++ utilities/helplist.py
@@ -3,7 +3,6 @@
 info = ['info', 'Get some info about the bot']
 addme = ['addme, invite', 'Shares a link that allows the bot to join your own discord server']
 commits = ['commits, commit', 'Shows the latest commit history pushed to github']
-leetcode = ['leetcode', 'Sends URL of random LeetCode problems']
 id = ['id', "returns numeric ID of a user, or the author's ID if no user is specified"]
 coinflip = ['coinflip', 'Flips a coin!']
 rolldice = ['rolldice', 'Rolls a die :game_die:']
@@ -14,6 +13,14 @@ eightball = ["eightball, 8ball, 8, ball", "The magic eight ball! :crystal_ball:"
 markdown = ["markdown", "Help on how to use markdown within Discord"]
 codeblock = ["codeblock", "makes user text into a 'fix' style codeblock"]
 mock = ["mock", "YoU CaN't JuSt PuT tHiS mEmE oN eVeRyThInG"]
-admin = ["admin commands: purge, kick, ban, unban", "A list of administration commands"]
+leetcode = ['leetcode', 'Sends URL of random LeetCode problems']
+purge = ["purge", "Deletes messages within the channel"]
+kick = ["kick", "Kicks a specified user"]
+ban = ["ban", "Bans a specified user"]
+unban = ["unban", "Unbans a specified user"]
 
-cogs = [info, addme, commits, leetcode, id, coinflip, rolldice, uppercase, lowercase, reverse, eightball, markdown, codeblock, admin]
+info_cmds = [info, addme, commits, id]
+rand_cmds = [info, addme, commits, leetcode, id, coinflip, 
+            rolldice, uppercase, lowercase, reverse, eightball, 
+            markdown, codeblock]
+admin_cmds = [purge, kick, ban, unban]
