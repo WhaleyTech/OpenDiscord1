3f6b1cfdd7753a82d54c57d4db5a6037e5d65933
evanblaine <evanblaineadams@gmail.com>
2020-09-03 11:34:10 -0500

    Update main.py
    Updated main and added "cogs" in config.example.json so it's configurable.

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-import os, json, discord
+import os
+import json
+import discord
 
 import discord.ext.commands as commands
 
 #if adding new commands, please refer to utilities/helplist.py
 class Main(commands.Bot):
-    
     def __init__(self):
         self.config = 'config.json'
         try:
             with open(self.config) as fp:
                 self.config = json.load(fp)
             print("Found config.json!")
-        except:
+        except Exception as e:
             print("Could not find config.json!")
-            
+            print(e)
+            print("=========================================")
+            exit(0)
+
         super().__init__(commands.when_mentioned_or(self.config['prefix']), description="Hello I'm OD1!")
 
-        for file in os.listdir("cogs"):
-            if file.endswith(".py"):
-                name = file[:-3]
-                self.load_extension(f"cogs.{name}")
-
 bot = Main()
+    
+for file in bot.config["cogs"]:
+    bot.load_extension(file)
+
 bot.run(bot.config['token'])
